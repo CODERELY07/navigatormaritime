220b2cdf32ede705274fda671c4583c20f568b00
CODERELY07 <164634420+CODERELY07@users.noreply.github.com>
2024-06-10 17:19:21 +0800

    done header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,73 @@
-<footer>
-
+<footer class="footer">
+    <div class="foot-wrap">
+            <div class="logos">
+                <img src="http://www.navigatormaritime.com/wp-content/uploads/2023/12/acc-logos-940x166.jpg" alt="logos">
+            </div>
+            <div class="widget-text">
+                <h2 class="light-color">NO OF VISITOR THIS YEAR</h2>
+            </div>
+            <div class="counter">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/8.gif" alt="">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/4.gif">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/7.gif">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/0.gif">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/5.gif">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/1.gif" alt="">
+            </div>
+            <div class="widget-text">
+                    <h2 class="light-color">OF VISITORS</h2>
+            </div>
+    </div>
+    <div class="black-footer d-flex text-center">
+        <div class="row contact-info">
+            <div class="col-md-12">
+                <h2 class="light-color mb-4">CONTACT US</h2>
+                <p>For more information and inquiries contact us on the following details below:</p>
+            </div>
+            <ul class="row contact-wrap">
+                <li class="col-md-4">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <h2 class="light-color-2 d-inline">Address</h2>
+                    <p>Unit 701 S&L Building 1500 Roxas Boulevard, 1000 Ermita, Manila</p>
+                </li>
+                <li class="col-md-4">
+                    <i class="fa-solid fa-phone"></i>
+                    <h2 class="light-color-2 d-inline">Contact NUMBERS</h2>
+                    <p>Phone: 8518-8634</p>
+                    <p>GLOBE: 0927-7920-799</p>
+                    <p>SMART: 0998-5944-089</p>
+                </li>
+                <li class="col-md-4">
+                    <i class="fa-solid fa-envelope"></i>
+                    <h2 class="light-color-2 d-inline">Email</h2>
+                    <p>marketing@navigatormaritime.com</p>
+                </li>
+            </ul>
+            <div class="col-md-12">
+                <a href="#">
+                    <div class="mx-auto send">
+                        <i class="fa-solid fa-pen"></i>
+                         SEND US A MESSAGE
+                    </div>
+                </a>
+            </div>
+            <div>
+                <span class="note">
+                © 2024 NAVIGATOR INTERNATIONAL MARITIME TRAINING AND ASSESSMENT CENTER INC ALL RIGHTS RESERVED.
+                <br>
+                TRADEMARKS AND BRANDS ARE THE PROPERTY OF THEIR RESPECTIVE OWNERS.
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="counter mx-auto">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/8.gif" alt="">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/4.gif">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/7.gif">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/0.gif">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/5.gif">
+                <img src="http://www.navigatormaritime.com/wp-content/plugins/hit-counter-max/designs/Digital/odmeter4/1.gif" alt="">
+    </div>
 </footer>
 
 <script src="js/script.js">
